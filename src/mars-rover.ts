5221d4d3ec83d37461cf8a1f1c5647da9bd54a5e
Masoud Azizpour <masoud.work@gmail.com>
2022-02-03 12:09:02 +0000

    Implement Moves

--- src/mars-rover.ts
+++ src/mars-rover.ts
@@ -5,6 +5,20 @@ enum Direction {
   West = "W",
 }
 
+const RightRotations = new Map<Direction, Direction>([
+  [Direction.North, Direction.East],
+  [Direction.East, Direction.South],
+  [Direction.South, Direction.West],
+  [Direction.West, Direction.North],
+]);
+
+const LeftRotations = new Map<Direction, Direction>([
+  [Direction.North, Direction.West],
+  [Direction.West, Direction.South],
+  [Direction.South, Direction.East],
+  [Direction.East, Direction.North],
+]);
+
 export class MarsRover {
   private x: number = 0;
   private y: number = 0;
@@ -12,20 +26,29 @@ export class MarsRover {
 
   public execute(command: string): string {
     const commands = command.split("");
-    commands.forEach(c => {
-      if (c === "R") {
-        this.rotateRight();
-      }
+    commands.forEach((c) => {
+      if (c === "R") this.rotateRight();
+
+      if (c === "L") this.rotateLeft();
+
+      if (c === "M") this.move();
     });
 
     return `${this.x}:${this.y}:${this.direction}`;
   }
 
-  private rotateRight()
-  {
-    if (this.direction === Direction.North) this.direction = Direction.East;
-    else if (this.direction === Direction.East) this.direction = Direction.South;
-    else if (this.direction === Direction.South) this.direction = Direction.West;
-    else if (this.direction === Direction.West) this.direction = Direction.North;
+  private rotateRight() {
+    this.direction = RightRotations.get(this.direction)!;
+  }
+
+  private rotateLeft() {
+    this.direction = LeftRotations.get(this.direction)!;
+  }
+
+  private move() {
+    if (this.direction === Direction.North) this.y++;
+    if (this.direction === Direction.East) this.x++;
+    if (this.direction === Direction.South) this.y--;
+    if (this.direction === Direction.West) this.x--;
   }
 }
--- test/mars-rover.test.ts
+++ test/mars-rover.test.ts
@@ -7,16 +7,55 @@ import { MarsRover } from "../src/mars-rover";
 // M moves the rover one grid square forward in the direction it is currently facing
 describe("MarsRover", () => {
   it.each`
-  direction   | command   | expectedPosition
-  ${"East"}   | ${"R"}    | ${"0:0:E"}
-  ${"South"}  | ${"RR"}   | ${"0:0:S"}
-  ${"West"}   | ${"RRR"}  | ${"0:0:W"}
-  ${"North"}  | ${"RRRR"} | ${"0:0:N"}
-  `("should rotate $direction when receives $command", ({ command, expectedPosition }) => {
-    const marsRover = new MarsRover();
+    direction  | command   | expectedPosition
+    ${"East"}  | ${"R"}    | ${"0:0:E"}
+    ${"South"} | ${"RR"}   | ${"0:0:S"}
+    ${"West"}  | ${"RRR"}  | ${"0:0:W"}
+    ${"North"} | ${"RRRR"} | ${"0:0:N"}
+  `(
+    "should rotate right towards $direction when receives $command",
+    ({ command, expectedPosition }) => {
+      const marsRover = new MarsRover();
 
-    const position = marsRover.execute(command);
+      const position = marsRover.execute(command);
 
-    expect(position).toBe(expectedPosition);
-  });
+      expect(position).toBe(expectedPosition);
+    }
+  );
+
+  it.each`
+    direction  | command   | expectedPosition
+    ${"West"}  | ${"L"}    | ${"0:0:W"}
+    ${"South"} | ${"LL"}   | ${"0:0:S"}
+    ${"East"}  | ${"LLL"}  | ${"0:0:E"}
+    ${"North"} | ${"LLLL"} | ${"0:0:N"}
+  `(
+    "should rotate left towards $direction when receives $command",
+    ({ command, expectedPosition }) => {
+      const marsRover = new MarsRover();
+
+      const position = marsRover.execute(command);
+
+      expect(position).toBe(expectedPosition);
+    }
+  );
+
+  it.each`
+    command      | expectedPosition
+    ${"M"}       | ${"0:1:N"}
+    ${"MM"}      | ${"0:2:N"}
+    ${"RM"}      | ${"1:0:E"}
+    ${"MRRM"}    | ${"0:0:S"}
+    ${"RMLLM"}   | ${"0:0:W"}
+    ${"MMRMMLM"} | ${"2:3:N"}
+  `(
+    "should move forward to $expectedPosition when receives $command",
+    ({ command, expectedPosition }) => {
+      const marsRover = new MarsRover();
+
+      const position = marsRover.execute(command);
+
+      expect(position).toBe(expectedPosition);
+    }
+  );
 });
