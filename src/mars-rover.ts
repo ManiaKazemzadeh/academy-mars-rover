a5bc0ebc56ada2b370cb2c914347ce6eca420282
Masoud Azizpour <masoud.work@gmail.com>
2022-02-03 10:23:26 +0000

    Add basic execute command

--- src/mars-rover.ts
+++ src/mars-rover.ts
@@ -1,6 +1,20 @@
+enum Direction {
+  North = "N",
+  East = "E",
+  South = "S",
+  West = "W",
+}
+
 export class MarsRover {
-  // hello changing this line for a test!!!!
-  // add(a: number, b: number) {
-  //   return a + b;
-  // }
+  private x: number = 0;
+  private y: number = 0;
+  private direction: Direction = Direction.North;
+
+  public execute(command: string): string {
+    if (command === "R") {
+      if (this.direction === Direction.North) this.direction = Direction.East;
+    }
+
+    return `${this.x}:${this.y}:${this.direction}`;
+  }
 }
